4c89dfbb8c28140e2a5f753b8eff9c41935115d3
denisSurkov <surkov0denis@yandex.ru>
2023-04-28 12:07:30 +0500

    first working prototype

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "config_reading.h"
 #include "sub_processes.h"
 
+int shouldRun = 1;
+
+void handleSighup(int sig) {
+    printf("hello from sighup");
+    shouldRun = 0;
+}
+
+
 int main(int argc, char * argv[]) {
     size_t returnProcCount = 0;
+
+    signal(SIGHUP, handleSighup);
 
     struct SubProcess ** processes = readConfig(argv[1], &returnProcCount);
 
@@ -13,6 +26,41 @@ int main(int argc, char * argv[]) {
             fprintf(stdout, "%s ", subProcess->configLineParams[j]);
         }
         fprintf(stdout, "\n");
+    }
+
+    pid_t cpid;
+    for (int i = 0; i < returnProcCount; ++i) {
+        struct SubProcess * subProcess = processes[i];
+        cpid = fork();
+
+        switch (cpid)
+        {
+            case -1:
+                // failed
+                printf("Fork failed; cpid == -1\n");
+                break;
+            case 0:
+                // child
+                cpid = getpid();         //global PID
+                execlp(subProcess->configLineParams[0], subProcess->configLineParams[0], subProcess->configLineParams[1], NULL);
+            default:
+                // parent
+                subProcess->pid = cpid;
+        }
+
+    }
+
+    while (shouldRun)
+    {
+        cpid=waitpid(-1, NULL, 0);   //ждем любого завершенного потомка
+        for (int p=0; p<returnProcCount; p++)
+        {
+            if(processes[p]->pid==cpid)
+            {
+                //делаем что-то по завершении дочернего процесса
+                printf("Child number %d pid %d finished\n",p,cpid);
+            }
+        }
     }
 
     return 0;
