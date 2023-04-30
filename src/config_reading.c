625f8c1173b21b701e5e8e6b9b91e84b90e55047
denisSurkov <surkov0denis@yandex.ru>
2023-04-30 21:33:05 +0500

    fixed reading lines from config file

--- src/config_reading.c
+++ src/config_reading.c
@@ -8,44 +8,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define LINE_DELIMITERS " \n"
 #define MAX_ARGUMENTS_IN_LINE 4096
 #define MAX_LINES_IN_FILE 1024
 
 struct SubProcess * processOneLine(char * line, size_t len) {
     char * lineArguments[MAX_ARGUMENTS_IN_LINE];
-    size_t lineArgumentsFirstFreeIndex = 0;
-    size_t segmentStartIndex = 0;
     size_t currentIndex = 0;
 
-    while (currentIndex < len) {
-        if (line[currentIndex] == ' ') {
-            size_t segmentLength = currentIndex - segmentStartIndex;
+    char * argument;
+    argument = strtok(line, LINE_DELIMITERS);
 
-            lineArguments[lineArgumentsFirstFreeIndex] = (char *) malloc(sizeof(char) * segmentLength);
-            strncpy(lineArguments[lineArgumentsFirstFreeIndex], &line[segmentStartIndex], segmentLength);
-
-            lineArgumentsFirstFreeIndex++;
-
-            while (currentIndex < len && line[currentIndex] == ' ') {
-                currentIndex++;
-            }
-            segmentStartIndex = currentIndex;
-            continue;
-        }
-
+    while (argument != NULL) {
+        lineArguments[currentIndex] = argument;
         currentIndex++;
+        argument = strtok(NULL, LINE_DELIMITERS);
     }
-
-    size_t segmentLength = currentIndex - segmentStartIndex;
-
-    lineArguments[lineArgumentsFirstFreeIndex] = (char *) malloc(sizeof(char) * segmentLength);
-    strncpy(lineArguments[lineArgumentsFirstFreeIndex], &line[segmentStartIndex], segmentLength);
-    lineArgumentsFirstFreeIndex++;
-
 
     struct SubProcess * subProcess =  malloc(sizeof(struct SubProcess));
     subProcess->pid = -1;
-    subProcess->paramsLength = lineArgumentsFirstFreeIndex;
+    subProcess->paramsLength = currentIndex;
     subProcess->configLineParams = lineArguments;
 
     return subProcess;
